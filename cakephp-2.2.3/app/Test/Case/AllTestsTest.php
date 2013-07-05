b14c6af6d7d0bbc0941dbe712c960b04f84ba026
jeffkolez <jeffkolez@gmail.com>
2013-07-05 10:25:03 -0400

    changed test

--- cakephp-2.2.3/app/Test/Case/AllTestsTest.php
+++ cakephp-2.2.3/app/Test/Case/AllTestsTest.php
@@ -1,9 +1,13 @@
 <?php
 
-class AllModelTest extends CakeTestSuite {
+class AllTestsTest extends PHPUnit_Framework_TestSuite {
+
     public static function suite() {
-        $suite = new CakeTestSuite('All model tests');
-        $suite->addTestDirectory(TESTS . 'Case' . DS . 'Helper');
+        $path = APP . 'Test' . DS . 'Case' . DS;
+        $suite = new CakeTestSuite('All tests');
+        $suite->addTestDirectory($path . 'Model' . DS);
         return $suite;
+
     }
+
 }
