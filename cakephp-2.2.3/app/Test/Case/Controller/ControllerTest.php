c45dd7512ec7f254ecc7f7be38f749f8fd950a85
jeffkolez <jeffkolez@gmail.com>
2013-07-05 11:34:47 -0400

    jeff is awesome

---
+++ cakephp-2.2.3/app/Test/Case/Controller/ControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+App::uses('Controller', 'Controller');
+
+class ControllerTest extends CakeTestCase {
+
+    public function setUp() {
+    	parent::setUp();
+    }
+
+    public function testCase() {
+		$jeffIsAwesome = true;
+    	$this->assertTrue($jeffIsAwesome);
+    }
+
+}
